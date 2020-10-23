772962b8e255e4e33053ff40df0579b9a762e27e
AD1024 <dh63@cs.washington.edu>
2020-10-23 14:33:23 -0700

    [ attemp ] fix runtime

--- src/runtime/contrib/vta_matmul/vta_matmul_runtime.cc
+++ src/runtime/contrib/vta_matmul/vta_matmul_runtime.cc
@@ -3,188 +3,363 @@
 namespace tvm {
 namespace runtime {
 namespace contrib {
-    extern "C" TVM_DLL void run_vta_simulator(float *acc, float *weight, int in_H, int in_W,
-                        int w_H, float *out_buf) {
-        const uint32_t num_instr = 7;
-        std::cerr << "in_W: " << in_W << " " << "in_H: " << in_H << "\n";
-        std::cerr << "w_H: " << w_H << std::endl;
-        std::cerr << "Input:\n";
-        for (int i = 0; i < in_H; ++i) {
-            for (int j = 0; j < in_W; ++j) {
-                std::cerr << acc[i * in_W + j] << " ";
-            }
-            std::cerr << "\n";
+
+typedef uint32_t uop_T;
+typedef int8_t wgt_T;
+typedef int8_t inp_T;
+typedef int8_t out_T;
+typedef int32_t acc_T;
+
+void* allocBuffer(size_t num_bytes) { return VTAMemAlloc(num_bytes, VTA_CACHED); }
+
+template <typename T>
+T ** alloc2dArray(int rows, int cols) {
+  T **array = static_cast<T **>(malloc(sizeof(T *) * rows));
+  for (int i = 0; i < rows; i++) {
+    array[i] = static_cast<T *>(malloc(sizeof(T) * cols));
+  }
+  return array;
+}
+
+VTAUop* getGEMMUops(int batch, int in_feat, int out_feat) {
+  // Derive the total uop size
+  int uop_size = batch;
+  VTAUop *uop_buf = static_cast<VTAUop *>(VTAMemAlloc(sizeof(VTAUop) * uop_size, VTA_CACHED));
+  for (int i = 0; i < batch; i++) {
+    uop_buf[i].dst_idx = i * out_feat;
+    uop_buf[i].src_idx = i * in_feat;
+    uop_buf[i].wgt_idx = 0;
+  }
+  return uop_buf;
+}
+
+template <typename DST_T, int DST_T_WIDTH, typename SRC_T, int SRC_T_WIDTH>
+void packBuffer(DST_T* dst, SRC_T** src, int y_size, int x_size, int y_block, int x_block) {
+  assert((SRC_T_WIDTH * x_block * y_block) % DST_T_WIDTH == 0);
+  assert(DST_T_WIDTH <= 64);
+  int buffer_idx = 0;
+  int ratio = DST_T_WIDTH / SRC_T_WIDTH;
+  long long int mask = (1ULL << SRC_T_WIDTH) - 1;
+  DST_T tmp = 0;
+  for (int i = 0; i < y_size / y_block; i++) {
+    for (int j = 0; j < x_size / x_block; j++) {
+      for (int k = 0; k < y_block; k++) {
+        for (int l = 0; l < x_block; l++) {
+          int block_idx = l + k * x_block;
+          tmp |= (src[i * y_block + k][j * x_block + l] & mask)
+                 << ((block_idx % ratio) * SRC_T_WIDTH);
+          // When tmp is packed, write to destination array
+          if (block_idx % ratio == ratio - 1) {
+            dst[buffer_idx++] = tmp;
+            tmp = 0;
+          }
         }
-        std::cerr << "\n";
-        for (int i = 0; i < w_H; ++i) {
-            for (int j = 0; j < in_W; ++j) {
-                std::cerr << weight[i * w_H + j] << " ";
-            }
-            std::cerr << "\n";
-        }
-        memset(out_buf, 0, sizeof(float) * in_H * w_H);
-        // LOAD UOP
-        // LOAD INP
-        // LOAD WGT
-        // GEMM
-        // ALU
-        // STORE
-        // FINISH
-        void *instr_buf;
-        void *mem_buf;
-        void *wgt_buf;
-        void *sram_out;
-        void *uop_buf;
-        memset(out_buf, 0, sizeof(float) * in_W * w_H);
-        VTADeviceHandle device_handle = VTADeviceAlloc();
-        instr_buf = VTAMemAlloc(sizeof(VTAGenericInsn) * num_instr, 0);
-        mem_buf = VTAMemAlloc((in_H * in_W) * sizeof(float), 0);
-        wgt_buf = VTAMemAlloc((in_W * w_H) * sizeof(float), 0);
-        sram_out = VTAMemAlloc((in_H * w_H) * sizeof(uint8_t), 0);
-        uop_buf = VTAMemAlloc(sizeof(VTAUop), 0);
-
-        float *mem_float = reinterpret_cast<float*>(mem_buf);
-        float *wgt_float = reinterpret_cast<float*>(wgt_buf);
-        VTAUop *mem_uop = reinterpret_cast<VTAUop*>(uop_buf);
-        uint8_t *sram_obuf = reinterpret_cast<uint8_t*>(sram_out);
-        std::cerr << "Initializing data" << std::endl;
-        for (int i = 0; i < in_H; ++i) {
-            for (int j = 0; j < in_W; ++j) {
-                mem_float[i * in_W + j] = acc[i * in_W + j];
-            }
-        }
-
-        for (int i = 0; i < in_W; ++i) {
-            for (int j = 0; j < w_H; ++j) {
-                wgt_float[i * w_H + j] = weight[i * w_H + j];
-            }
-        }
-        
-        VTAMemInsn *instr_mem = reinterpret_cast<VTAMemInsn*>(instr_buf);
-        VTAGemInsn *instr_gem = reinterpret_cast<VTAGemInsn*>(instr_buf);
-        VTAAluInsn *instr_alu = reinterpret_cast<VTAAluInsn*>(instr_buf);
-
-        std::cerr << "Define instructions" << std::endl;
-
-        mem_uop->dst_idx = 0;
-        mem_uop->src_idx = 0;
-        mem_uop->wgt_idx = 0;
-
-        instr_mem[0].opcode = VTA_OPCODE_LOAD;
-        instr_mem[0].memory_type = VTA_MEM_ID_UOP;
-        instr_mem[0].x_size = sizeof(VTAUop);
-        instr_mem[0].y_size = sizeof(VTAUop);
-        instr_mem[0].sram_base = 0;
-        instr_mem[0].dram_base = VTAMemGetPhyAddr(mem_uop) / VTA_UOP_ELEM_BYTES;
-        instr_mem[0].y_pad_0 = 0;
-        instr_mem[0].y_pad_1 = 0;
-        instr_mem[0].x_pad_0 = 0;
-        instr_mem[0].x_pad_1 = 0;
-        instr_mem[0].push_prev_dep = 0;
-        instr_mem[0].pop_prev_dep = 0;
-        instr_mem[0].push_next_dep = 0;
-        instr_mem[0].pop_prev_dep = 0;
-
-        instr_mem[1].opcode = VTA_OPCODE_LOAD;
-        instr_mem[1].memory_type = VTA_MEM_ID_INP;
-        instr_mem[1].sram_base = 0;
-        instr_mem[1].dram_base = VTAMemGetPhyAddr(mem_buf) / VTA_INP_ELEM_BYTES;
-        instr_mem[1].x_size = in_W;
-        instr_mem[1].y_size = in_H;
-        instr_mem[1].y_pad_0 = 0;
-        instr_mem[1].y_pad_1 = 0;
-        instr_mem[1].x_pad_0 = 0;
-        instr_mem[1].x_pad_1 = 0;
-        instr_mem[1].x_stride = in_W;
-        instr_mem[1].push_prev_dep = 0;
-        instr_mem[1].pop_prev_dep = 0;
-        instr_mem[1].push_next_dep = 0;
-        instr_mem[1].pop_next_dep = 0;
-
-        instr_mem[2].opcode = VTA_OPCODE_LOAD;
-        instr_mem[2].memory_type = VTA_MEM_ID_WGT;
-        instr_mem[2].sram_base = 0;
-        instr_mem[2].dram_base = VTAMemGetPhyAddr(wgt_float) / VTA_WGT_ELEM_BYTES;
-        instr_mem[2].x_size = w_H;
-        instr_mem[2].y_size = in_W;
-        instr_mem[2].y_pad_0 = 0;
-        instr_mem[2].y_pad_1 = 0;
-        instr_mem[2].x_pad_0 = 0;
-        instr_mem[2].x_pad_1 = 0;
-        instr_mem[2].x_stride = w_H;
-        instr_mem[2].push_prev_dep = 0;
-        instr_mem[2].pop_prev_dep = 0;
-        instr_mem[2].push_next_dep = 1;
-        instr_mem[2].pop_prev_dep = 0;
-
-        instr_gem[3].opcode = VTA_OPCODE_GEMM;
-        instr_gem[3].pop_prev_dep = 1;
-        instr_gem[3].pop_next_dep = 0;
-        instr_gem[3].push_next_dep = 0;
-        instr_gem[3].push_prev_dep = 0;
-        instr_gem[3].uop_bgn = 0;
-        instr_gem[3].uop_end = 1;
-        instr_gem[3].dst_factor_in = 0;
-        instr_gem[3].dst_factor_out = 0;
-        instr_gem[3].src_factor_in = 0;
-        instr_gem[3].src_factor_out = 0;
-        instr_gem[3].wgt_factor_in = 0;
-        instr_gem[3].wgt_factor_out = 0;
-        instr_gem[3].iter_in = 1;
-        instr_gem[3].iter_out = 1;
-
-        instr_alu[4].opcode = VTA_OPCODE_ALU;
-        instr_alu[4].alu_opcode = VTA_ALU_OPCODE_ADD;
-        instr_alu[4].uop_bgn = 0;
-        instr_alu[4].uop_end = 1;
-        instr_alu[4].use_imm = true;
-        instr_alu[4].imm = 0;
-        instr_alu[4].pop_prev_dep = 0;
-        instr_alu[4].pop_next_dep = 0;
-        instr_alu[4].push_prev_dep = 0;
-        instr_alu[4].push_next_dep = 1;
-        instr_alu[4].iter_in = 1;
-        instr_alu[4].iter_out = 1;
-        instr_alu[4].reset_reg = false;
-        instr_alu[4].dst_factor_out = 0;
-        instr_alu[4].src_factor_out = 0;
-        instr_alu[4].dst_factor_in = 0;
-        instr_alu[4].src_factor_in = 0;
-
-        instr_mem[5].opcode = VTA_OPCODE_STORE;
-        instr_mem[5].memory_type = VTA_MEM_ID_OUT;
-        instr_mem[5].sram_base = 0;
-        instr_mem[5].dram_base = VTAMemGetPhyAddr(sram_obuf) / VTA_OUT_ELEM_BYTES;
-        instr_mem[5].pop_prev_dep = 1;
-        instr_mem[5].push_prev_dep = 1;
-        instr_mem[5].pop_next_dep = 0;
-        instr_mem[5].push_next_dep = 0;
-        instr_mem[5].x_stride = 0;
-        instr_mem[5].y_pad_0 = 0;
-        instr_mem[5].y_pad_1 = 0;
-        instr_mem[5].x_pad_0 = 0;
-        instr_mem[5].x_pad_1 = 0;
-
-        instr_gem[6].opcode = VTA_OPCODE_FINISH;
-        instr_gem[6].pop_prev_dep = 0;
-        instr_gem[6].pop_next_dep = 1;
-        instr_gem[6].push_prev_dep = 0;
-        instr_gem[6].push_next_dep = 0;
-
-        std::cerr << "Run" << std::endl;
-
-        VTADeviceRun(device_handle, VTAMemGetPhyAddr(instr_buf), num_instr, 10);
-        for (int i = 0; i < in_H * w_H; ++i) {
-            out_buf[i] = static_cast<float>(sram_obuf[i]);
-            std::cerr << i << ": " << sram_obuf[i] << std::endl;
-        }
-
-        VTAMemFree(mem_buf);
-        VTAMemFree(sram_out);
-        VTAMemFree(wgt_buf);
-        VTAMemFree(instr_buf);
-        VTADeviceFree(device_handle);
+      }
     }
+  }
 }
+
+template <typename T>
+T** allocInit2dArray(int rows, int cols, bool init_data, float* from, T data = 0) {
+  // Allocate
+  T** array = static_cast<T**>(malloc(sizeof(T*) * rows));
+  for (int i = 0; i < rows; i++) {
+    array[i] = static_cast<T*>(malloc(sizeof(T) * cols));
+  }
+  // Init
+  if (init_data) {
+    for (int i = 0; i < rows; i++) {
+      for (int j = 0; j < cols; j++) {
+        if (from == nullptr) {
+          array[i][j] = static_cast<T>(data);
+        } else {
+          array[i][j] = static_cast<T>(from[i * cols + j]);
+        }
+      }
+    }
+  }
+  return array;
 }
+
+template <typename DST_T, int DST_T_WIDTH, typename SRC_T, int SRC_T_WIDTH>
+void unpackBuffer(DST_T **dst, SRC_T *src, int y_size, int x_size, int y_block, int x_block) {
+  assert((DST_T_WIDTH * x_block * y_block) % SRC_T_WIDTH == 0);
+  int buffer_idx = 0;
+  long long int mask = (1ULL << DST_T_WIDTH) - 1;
+  int ratio = SRC_T_WIDTH / DST_T_WIDTH;
+  for (int i = 0; i < y_size / y_block; i++) {
+    for (int j = 0; j < x_size / x_block; j++) {
+      for (int k = 0; k < y_block; k++) {
+        for (int l = 0; l < x_block; l++) {
+          int block_idx = l + k * x_block;
+          dst[i * y_block + k][j * x_block + l] = (src[buffer_idx] >> ((block_idx % ratio) * DST_T_WIDTH)) & mask;
+          if (block_idx % ratio == ratio - 1) {
+            buffer_idx++;
+          }
+        }
+      }
+    }
+  }
 }
+
+
+VTAGenericInsn get2DLoadStoreInsn(int opcode, int type, int sram_offset, int dram_offset,
+                                  int y_size, int x_size, int x_stride, int y_pad, int x_pad,
+                                  int pop_prev_dep, int pop_next_dep, int push_prev_dep,
+                                  int push_next_dep) {
+  // Converter
+  union VTAInsn converter;
+  // Memory instruction initialization
+  VTAMemInsn insn = {};
+  insn.opcode = opcode;
+  insn.pop_prev_dep = pop_prev_dep;
+  insn.pop_next_dep = pop_next_dep;
+  insn.push_prev_dep = push_prev_dep;
+  insn.push_next_dep = push_next_dep;
+  insn.memory_type = type;
+  insn.sram_base = sram_offset;
+  insn.dram_base = dram_offset;
+  insn.y_size = y_size;
+  insn.x_size = x_size;
+  insn.x_stride = x_stride;
+  insn.y_pad_0 = y_pad;
+  insn.y_pad_1 = y_pad;
+  insn.x_pad_0 = x_pad;
+  insn.x_pad_1 = x_pad;
+  converter.mem = insn;
+  return converter.generic;
+}
+
+VTAGenericInsn get1DLoadStoreInsn(int opcode, int type, int sram_offset, int dram_offset, int size,
+                                  int pop_prev_dep, int pop_next_dep, int push_prev_dep,
+                                  int push_next_dep) {
+  // Converter
+  union VTAInsn converter;
+  // Memory instruction initialization
+  VTAMemInsn insn = {};
+  insn.opcode = opcode;
+  insn.pop_prev_dep = pop_prev_dep;
+  insn.pop_next_dep = pop_next_dep;
+  insn.push_prev_dep = push_prev_dep;
+  insn.push_next_dep = push_next_dep;
+  insn.memory_type = type;
+  insn.sram_base = sram_offset;
+  insn.dram_base = dram_offset;
+  insn.y_size = 1;
+  insn.x_size = size;
+  insn.x_stride = size;
+  insn.y_pad_0 = 0;
+  insn.y_pad_1 = 0;
+  insn.x_pad_0 = 0;
+  insn.x_pad_1 = 0;
+  converter.mem = insn;
+  return converter.generic;
+}
+
+VTAGenericInsn getGEMMInsn(int uop_offset, int batch, int in_feat, int out_feat,
+                           bool uop_compression, int pop_prev_dep, int pop_next_dep,
+                           int push_prev_dep, int push_next_dep) {
+  // Converter
+  union VTAInsn converter;
+  // GEMM instruction initialization
+  VTAGemInsn insn;
+  insn.opcode = VTA_OPCODE_GEMM;
+  insn.pop_prev_dep = pop_prev_dep;
+  insn.pop_next_dep = pop_next_dep;
+  insn.push_prev_dep = push_prev_dep;
+  insn.push_next_dep = push_next_dep;
+  insn.reset_reg = false;
+  if (!uop_compression) {
+    insn.uop_bgn = uop_offset;
+    insn.uop_end = uop_offset + batch * in_feat * out_feat;
+    insn.iter_out = 1;
+    insn.iter_in = 1;
+    insn.dst_factor_out = 0;
+    insn.src_factor_out = 0;
+    insn.wgt_factor_out = 0;
+    insn.dst_factor_in = 0;
+    insn.src_factor_in = 0;
+    insn.wgt_factor_in = 0;
+  } else {
+    insn.uop_bgn = uop_offset;
+    insn.uop_end = uop_offset + batch;
+    insn.iter_out = in_feat;
+    insn.iter_in = out_feat;
+    insn.dst_factor_out = 0;
+    insn.src_factor_out = 1;
+    insn.wgt_factor_out = 1;
+    insn.dst_factor_in = 1;
+    insn.src_factor_in = 0;
+    insn.wgt_factor_in = in_feat;
+  }
+  converter.gemm = insn;
+  return converter.generic;
+}
+
+VTAGenericInsn getFinishInsn(bool pop_prev, bool pop_next) {
+  // Converter
+  union VTAInsn converter;
+  // GEMM instruction initialization
+  VTAGemInsn insn;
+  insn.opcode = VTA_OPCODE_FINISH;
+  insn.pop_prev_dep = pop_prev;
+  insn.pop_next_dep = pop_next;
+  insn.push_prev_dep = 0;
+  insn.push_next_dep = 0;
+  insn.reset_reg = false;
+  insn.uop_bgn = 0;
+  insn.uop_end = 0;
+  insn.iter_out = 0;
+  insn.iter_in = 0;
+  insn.dst_factor_out = 0;
+  insn.src_factor_out = 0;
+  insn.wgt_factor_out = 0;
+  insn.dst_factor_in = 0;
+  insn.src_factor_in = 0;
+  insn.wgt_factor_in = 0;
+  converter.gemm = insn;
+  return converter.generic;
+}
+
+extern "C" TVM_DLL void run_vta_simulator(float* input, float* weight, int batch, int in_channels,
+                                          int out_channels, float* out_buf) {
+  const uint32_t num_instr = 7;
+  std::cerr << "in_W: " << in_channels << " "
+            << "in_H: " << batch << "\n";
+  std::cerr << "w_H: " << out_channels << std::endl;
+  // std::cerr << "Input:\n";
+  // for (int i = 0; i < batch; ++i) {
+  //   for (int j = 0; j < in_channels; ++j) {
+  //     std::cerr << input[i * in_channels + j] << " ";
+  //   }
+  //   std::cerr << "\n";
+  // }
+  // std::cerr << "\n";
+  // for (int i = 0; i < out_channels; ++i) {
+  //   for (int j = 0; j < in_channels; ++j) {
+  //     std::cerr << weight[i * out_channels + j] << " ";
+  //   }
+  //   std::cerr << "\n";
+  // }
+  int uop_size = batch / VTA_BATCH;
+  int inp_size = batch / VTA_BATCH * in_channels / VTA_BLOCK_IN;
+  int wgt_size = in_channels / VTA_BLOCK_IN * out_channels / VTA_BLOCK_OUT;
+  int out_size = batch / VTA_BATCH * out_channels / VTA_BLOCK_OUT;
+  memset(out_buf, 0, sizeof(float) * out_channels * in_channels);
+  // LOAD UOP
+  // LOAD INP
+  // LOAD WGT
+  // GEMM
+  // ALU
+  // STORE
+  // FINISH
+  VTADeviceHandle device_handle = VTADeviceAlloc();
+  VTAGenericInsn *instr_buf =
+    static_cast<VTAGenericInsn *>(allocBuffer(sizeof(VTAGenericInsn) * num_instr));
+
+  inp_T** inputs = allocInit2dArray<inp_T>(batch, in_channels, 1, input);
+  wgt_T** weights = allocInit2dArray<wgt_T>(out_channels, in_channels, 1, weight);
+  acc_T** bias = allocInit2dArray<acc_T>(batch, out_channels, 0, nullptr, 0);
+  VTAUop* uops =
+      getGEMMUops(batch / VTA_BATCH, in_channels / VTA_BLOCK_IN, out_channels / VTA_BLOCK_OUT);
+
+  std::cerr << "Inp Size: " << inp_size << " " << "ELEM BYTES: " << VTA_INP_ELEM_BYTES << std::endl;
+
+  uint32_t* input_buf = static_cast<uint32_t*>(allocBuffer(VTA_INP_ELEM_BYTES * inp_size));
+  packBuffer<uint32_t, 32, inp_T, VTA_INP_WIDTH>(input_buf, inputs, batch, in_channels, VTA_BATCH,
+                                                 VTA_BLOCK_IN);
+  uint32_t* wgt_buf = static_cast<uint32_t*>(allocBuffer(VTA_WGT_ELEM_BYTES * inp_size));
+  packBuffer<uint32_t, 32, out_T, VTA_WGT_WIDTH>(wgt_buf, weights, out_channels, in_channels,
+                                                 VTA_BLOCK_OUT, VTA_BLOCK_IN);
+
+  uint32_t* bias_buf = static_cast<uint32_t*>(allocBuffer(VTA_ACC_ELEM_BYTES * out_size));
+  packBuffer<uint32_t, 32, acc_T, VTA_OUT_WIDTH>(bias_buf, bias, batch, out_channels, VTA_BATCH,
+                                                 VTA_BLOCK_OUT);
+  uint32_t* output_buf = static_cast<uint32_t*>(allocBuffer(VTA_OUT_ELEM_BYTES * out_size));
+
+  std::cerr << "Define instructions" << std::endl;
+
+  int ptr = 0;
+
+  instr_buf[ptr++] = get1DLoadStoreInsn(
+    VTA_OPCODE_LOAD,
+    VTA_MEM_ID_UOP,
+    0,
+    VTAMemGetPhyAddr(uops) / VTA_UOP_ELEM_BYTES,
+    uop_size,
+    0, 0, 0, 0
+  );
+
+  instr_buf[ptr++] = get1DLoadStoreInsn(
+    VTA_OPCODE_LOAD,
+    VTA_MEM_ID_ACC,
+    0,
+    VTAMemGetPhyAddr(bias_buf) / VTA_ACC_ELEM_BYTES,
+    out_size,
+    0, 0, 1, 0
+  );
+
+  instr_buf[ptr++] = get1DLoadStoreInsn(
+    VTA_OPCODE_LOAD,
+    VTA_MEM_ID_WGT,
+    0,
+    VTAMemGetPhyAddr(wgt_buf) / VTA_WGT_ELEM_BYTES,
+    wgt_size,
+    0, 1, 0, 0
+  );
+
+  instr_buf[ptr++] = get1DLoadStoreInsn(
+    VTA_OPCODE_LOAD,
+    VTA_MEM_ID_INP,
+    0,
+    VTAMemGetPhyAddr(input_buf) / VTA_INP_ELEM_BYTES,
+    inp_size,
+    0, 0, 0, 1
+  );
+
+  instr_buf[ptr++] = getGEMMInsn(
+    0,
+    batch / VTA_BATCH,
+    in_channels / VTA_BLOCK_IN,
+    out_channels / VTA_BLOCK_OUT,
+    1,
+    1, 0, 0, 1
+  );
+
+  instr_buf[ptr++] = get1DLoadStoreInsn(
+    VTA_OPCODE_STORE,
+    VTA_MEM_ID_OUT,
+    0,
+    VTAMemGetPhyAddr(output_buf) / VTA_OUT_ELEM_BYTES,
+    out_size,
+    1, 0, 1, 0
+  );
+
+  instr_buf[ptr++] = getFinishInsn(0, 1);
+
+  std::cerr << "Run" << std::endl;
+
+  VTADeviceRun(device_handle, VTAMemGetPhyAddr(instr_buf), num_instr, 10);
+
+  out_T **outputs = alloc2dArray<out_T>(batch, out_channels);
+  unpackBuffer<out_T, VTA_OUT_WIDTH, uint32_t, 32>(outputs,
+                                                   output_buf,
+                                                   batch,
+                                                   out_channels,
+                                                   VTA_BATCH,
+                                                   VTA_BLOCK_OUT);
+
+  for (int i = 0; i < batch; ++i) {
+    for (int j = 0; j < out_channels; ++j) {
+      out_buf[i * out_channels + j] = static_cast<float>(outputs[i][j]);
+    }
+  }
+  VTAMemFree(input_buf);
+  VTAMemFree(output_buf);
+  VTAMemFree(wgt_buf);
+  VTAMemFree(instr_buf);
+  VTADeviceFree(device_handle);
+}
+}  // namespace contrib
+}  // namespace runtime
+}  // namespace tvm
