2346debcb48933d938a4105e5bb9546459a93633
Bo-Yuan Huang <byhuang1992@gmail.com>
2021-01-27 08:35:24 +0000

    TODO: fill in ILA assembly translation pipeline

--- src/runtime/contrib/ilaflex/ilaflex_runtime.cc
+++ src/runtime/contrib/ilaflex/ilaflex_runtime.cc
@@ -23,76 +23,113 @@ class ILAFlexRuntime : public JSONRuntimeBase {
                  const Array<String> const_names)
       : JSONRuntimeBase(symbol_name, graph_json, const_names) {}
 
-  const char* type_key() const { return "ilaflex"; }  // namespace contrib
+  const char* type_key() const { return "ilaflex"; }
 
   void Init(const Array<NDArray>& consts) override {
-    CHECK(consts.size() == 0) << "matmul should have no consts";
+    // CHECK(consts.size() == 0);
   }
 
   void Run() override {
     CHECK(symbol_name_.substr(0, 7) == "ilaflex") << symbol_name_;
     LOG(INFO) << "[Runtime] enter " << symbol_name_ << " runtime";
 
-    if (outputs_.size() == 1 &&
+    if (outputs_.size() == 1 && input_nodes_.size() == 3 &&
         nodes_[outputs_[0].id_].GetOpName() == "ilaflex.linear") {
-      LOG(INFO) << "[Runtime] off-loading ilaflex.linear";
+      /*
+       * out = bias_add(batch_matmul(x, y), z)
+       *
+       * input x:
+       *  - dimension: (x_dim_0, x_dim_1, x_dim_2)
+       *  - data: x_data_ptr, x_data_size
+       *
+       * input y:
+       *  - dimension: (y_dim_0, y_dim_1, y_dim_2)
+       *  - data: y_data_ptr, y_data_size
+       *
+       * input z:
+       *  - dimension: (z_dim_0)
+       *  - data: z_data_ptr, z_data_size
+       *
+       * output:
+       *  - dimension: (o_dim_0, o_dim_1, o_dim_2)
+       *  - data: o_data_ptr, o_data_size
+       */
 
-      // get input node data
-      for (size_t i = 0; i < input_nodes_.size(); ++i) {
-        auto eid = EntryID(input_nodes_[i], 0);
-        auto& node_data = data_entry_[eid];
+      // x
+      auto eid_x = EntryID(input_nodes_[0], 0);
+      auto& node_data_x = data_entry_[eid_x];
+      CHECK(node_data_x->ndim == 3);
+      auto x_dim_0 = node_data_x->shape[0];
+      auto x_dim_1 = node_data_x->shape[1];
+      auto x_dim_2 = node_data_x->shape[2];
+      auto x_data_size = GetDataSize(*node_data_x);
+      char* x_data_ptr = new char[x_data_size];
+      std::copy(reinterpret_cast<char*>(node_data_x->data),
+                reinterpret_cast<char*>(node_data_x->data) + x_data_size,
+                x_data_ptr);
 
-        auto ndim = node_data->ndim;
-        // CHECK(ndim == 3) << "batch_matmul input dimension: " << ndim;
-        LOG(INFO) << "Input " << eid << " dim: " << ndim;
+      // y
+      auto eid_y = EntryID(input_nodes_[1], 0);
+      auto& node_data_y = data_entry_[eid_y];
+      CHECK(node_data_y->ndim == 3);
+      auto y_dim_0 = node_data_y->shape[0];
+      auto y_dim_1 = node_data_y->shape[1];
+      auto y_dim_2 = node_data_y->shape[2];
+      auto y_data_size = GetDataSize(*node_data_y);
+      char* y_data_ptr = new char[y_data_size];
+      std::copy(reinterpret_cast<char*>(node_data_y->data),
+                reinterpret_cast<char*>(node_data_y->data) + y_data_size,
+                y_data_ptr);
 
-        LOG(INFO) << "[Runtime-TODO] virtual store node " << eid << " data:";
-        auto buffer_size = GetDataSize(*node_data);
-        char* dst = new char[buffer_size];
-        std::copy(reinterpret_cast<char*>(node_data->data),
-                  reinterpret_cast<char*>(node_data->data) + buffer_size, dst);
+      // z
+      auto eid_z = EntryID(input_nodes_[2], 0);
+      auto& node_data_z = data_entry_[eid_z];
+      CHECK(node_data_z->ndim == 1);
+      auto z_dim_0 = node_data_z->shape[0];
+      auto z_data_size = GetDataSize(*node_data_z);
+      char* z_data_ptr = new char[z_data_size];
+      std::copy(reinterpret_cast<char*>(node_data_z->data),
+                reinterpret_cast<char*>(node_data_z->data) + z_data_size,
+                z_data_ptr);
 
-        std::string data_str = "";
-        for (size_t j = 0; j < buffer_size; j++) {
-          data_str = data_str + " " + std::to_string((uint8_t)(dst[j]));
-        }
-        LOG(INFO) << "[Runtime-TODO]   <" << data_str << ">";
+      // output
+      auto eid_o = outputs_[0].id_;
+      auto node_data_o = data_entry_[eid_o];
+      CHECK(node_data_o->ndim == 3);
+      auto o_dim_0 = node_data_o->shape[0];
+      auto o_dim_1 = node_data_o->shape[1];
+      auto o_dim_2 = node_data_o->shape[2];
+      auto o_data_size = GetDataSize(*node_data_o);
+      char* o_data_ptr = new char[o_data_size];
 
-#if 0
-        for (auto dim = 0; dim < data_entry_[eid]->ndim; dim++) {
-          LOG(INFO) << "shape: " << data_entry_[eid]->shape[dim];
-        }
-#endif
-      }
-
+      /* TODO
+       *  - FlexNLP ILA simulator is available in $PATH as "flexnlp_ila_sim"
+       *  - generate tensor-level assembly
+       *  - generate data library
+       *  - translate to ILA instruction program fragment
+       *  - invoke the ILA simulator
+       *  - read back the result and store to o_data_ptr
+       */
       // call ILAng-generated simulator
-      std::string simulator = "/root/ilasim/flex";
       std::string command = "echo \"call assembly helper\"";
       auto res = std::system(command.c_str());
       CHECK(res == 0) << "Error executing simulator " << command;
 
-      // reads back the output
-      auto output_node_id = outputs_[0].id_;
-      auto output_node_data = data_entry_[output_node_id];
+#if 0
+      LOG(INFO) << x_dim_0 << ", " << x_dim_1 << ", " << x_dim_2;
+      LOG(INFO) << y_dim_0 << ", " << y_dim_1 << ", " << y_dim_2;
+      LOG(INFO) << z_dim_0;
+      LOG(INFO) << o_dim_0 << ", " << o_dim_1 << ", " << o_dim_2;
+#endif
 
-      {
-        LOG(INFO) << "[Runtime-TODO] read back simulation results (fake):";
-        auto buffer_size = GetDataSize(*output_node_data);
-        char* src = new char[buffer_size];
-        std::copy(src, src + buffer_size,
-                  reinterpret_cast<char*>(output_node_data->data));
-        std::string data_str = "";
-        for (size_t j = 0; j < buffer_size; j++) {
-          data_str = data_str + " " + std::to_string((uint8_t)(src[j]));
-        }
-        LOG(INFO) << "[Runtime-TODO]  <" << data_str << ">";
-      }
-
-      LOG(INFO) << "[Runtime] resume execution";
+      // copy the result and resume
+      std::copy(o_data_ptr, o_data_ptr + o_data_size,
+                reinterpret_cast<char*>(node_data_o->data));
 
     } else {
       LOG(FATAL) << "Unknown pattern " << symbol_name_;
     }
+    LOG(INFO) << "[Runtime] exit " << symbol_name_ << " runtime, resume host";
   }
 
  protected:
