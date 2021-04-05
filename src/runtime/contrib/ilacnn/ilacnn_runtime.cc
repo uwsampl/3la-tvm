c6072ee6690e7ba71d5b002fbea3e6affc35daab
Yi Li <leeoh12784@gmail.com>
2021-04-05 00:17:46 -0400

    merged from steve's hlscnn

---
+++ src/runtime/contrib/ilacnn/ilacnn_runtime.cc
@@ -0,0 +1,152 @@
+#include <tvm/node/container.h>
+#include <tvm/node/reflection.h>
+#include <tvm/runtime/container.h>
+#include <tvm/runtime/ndarray.h>
+#include <tvm/runtime/registry.h>
+#include <tvm/tir/op.h>
+
+#include <cstddef>
+#include <cstdlib>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <iomanip>
+#include <fstream>
+#include <sstream>
+
+#include "../json/json_node.h"
+#include "../json/json_runtime.h"
+
+namespace tvm {
+namespace runtime {
+namespace contrib {
+
+using namespace tvm::runtime;
+using namespace tvm::runtime::json;
+
+class IlaCNNRuntime : public JSONRuntimeBase {
+ public:
+  IlaCNNRuntime(const std::string& symbol_name, const std::string& graph_json,
+                 const Array<String> const_names)
+      : JSONRuntimeBase(symbol_name, graph_json, const_names) {}
+
+  const char* type_key() const { return "ilacnn"; }
+
+  void Init(const Array<NDArray>& consts) override {
+    // CHECK(consts.size() == 0);
+  }
+
+  void Run() override {
+    CHECK(symbol_name_.substr(0, 6) == "ilacnn") << symbol_name_;
+    LOG(INFO) << "[Runtime] entering " << symbol_name_ << " runtime";
+
+    if (outputs_.size() == 1 && input_nodes_.size() == 2 &&
+        nodes_[outputs_[0].id_].GetOpName() == "ilacnn.conv2d") {
+      auto call_node = nodes_[outputs_[0].id_];
+
+      // data
+      auto eid_data = EntryID(input_nodes_[0], 0);
+      auto& data_info = data_entry_[eid_data];
+      CHECK(data_info->ndim == 4);
+      std::cout << "Data shape: ("
+                << data_info->shape[0] << ", "
+                << data_info->shape[1] << ", "
+                << data_info->shape[2] << ", "
+                << data_info->shape[3]
+                << ")" << std::endl;
+
+      // weight
+      auto eid_weight = EntryID(input_nodes_[1], 0);
+      auto& weight_info = data_entry_[eid_weight];
+      CHECK(weight_info->ndim == 4);
+      std::cout << "Weight shape: ("
+                << weight_info->shape[0] << ", "
+                << weight_info->shape[1] << ", "
+                << weight_info->shape[2] << ", "
+                << weight_info->shape[3]
+                << ")" << std::endl;
+
+      // output
+      auto eid_o = outputs_[0].id_;
+      auto out_info = data_entry_[eid_o];
+      CHECK(out_info->ndim == 4);
+      std::cout << "Output shape: ("
+                << out_info->shape[0] << ", "
+                << out_info->shape[1] << ", "
+                << out_info->shape[2] << ", "
+                << out_info->shape[3]
+                << ")" << std::endl;
+
+      // attributes
+      auto strides = call_node.GetAttr<std::vector<std::string>>("strides");
+      auto padding = call_node.GetAttr<std::vector<std::string>>("padding");
+      auto data_layout = call_node.GetAttr<std::vector<std::string>>("data_layout");
+      auto kernel_layout = call_node.GetAttr<std::vector<std::string>>("kernel_layout");
+      // etc
+
+      std::cout << "Strides: " << "(";
+      for (const auto dim : strides) {
+        std::cout << dim << ",";
+      }
+      std::cout << ")" << std::endl;
+      std::cout << "Padding: " << "(";
+      for (const auto dim : padding) {
+        std::cout << dim << ",";
+      }
+      std::cout << ")" << std::endl;
+      std::cout << "Data layout: " << data_layout[0] << std::endl;
+      std::cout << "Kernel layout: " << kernel_layout[0] << std::endl;
+
+      // TODO: Instantiate and call driver
+    } else {
+      LOG(FATAL) << "Unknown pattern " << symbol_name_;
+    }
+    LOG(INFO) << "[Runtime] exit " << symbol_name_ << " runtime, resume host";
+  }
+
+  void dump_data(float* data_ptr, unsigned long& size, std::string path) {
+    std::ofstream fout;
+    std::stringstream ss;
+    fout.open(path, std::ios::out | std::ios::trunc);
+    for (auto i = 0; i < size; ++i) {
+      ss << data_ptr[i] << '\n';
+    }
+    fout << ss.rdbuf();
+    fout.close();
+  }
+
+  void retrieve_result(float* data_ptr, unsigned long& size, std::string path) {
+    // retrieve flexnlp results
+    std::ifstream fin;
+    fin.open(path, std::ios::in);
+    std::string float_str;
+    unsigned long cntr = 0;
+
+    while(std::getline(fin, float_str)) {
+      if (cntr >= size) {
+        LOG(FATAL) << "wrong number of elements in the result tensor";
+      }
+      data_ptr[cntr] = std::stof(float_str);
+      ++cntr;
+    }
+  }
+
+ protected:
+ private:
+};  // namespace runtime
+
+runtime::Module IlaCNNRuntimeCreate(String symbol_name, String graph_json,
+                                     const Array<String>& const_names) {
+  auto n = make_object<IlaCNNRuntime>(symbol_name, graph_json, const_names);
+  return runtime::Module(n);
+}
+
+TVM_REGISTER_GLOBAL("runtime.IlaCNNRuntimeCreate")
+    .set_body_typed(IlaCNNRuntimeCreate);
+
+TVM_REGISTER_GLOBAL("runtime.module.loadbinary_ilacnn")
+    .set_body_typed(JSONRuntimeBase::LoadFromBinary<IlaCNNRuntime>);
+
+}  // namespace contrib
+}  // namespace runtime
+}  // namespace tvm
