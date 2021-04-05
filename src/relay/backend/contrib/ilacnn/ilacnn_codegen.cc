e2afd986280832c8bd3ecae10f627a01b019c2a9
Yi Li <leeoh12784@gmail.com>
2021-04-05 00:17:46 -0400

    merged from steve's hlscnn

---
+++ src/relay/backend/contrib/ilacnn/ilacnn_codegen.cc
@@ -0,0 +1,98 @@
+#include <tvm/relay/attrs/nn.h>
+#include <tvm/relay/expr_functor.h>
+#include <tvm/relay/transform.h>
+#include <tvm/relay/type.h>
+#include <tvm/runtime/module.h>
+#include <tvm/runtime/registry.h>
+
+#include <fstream>
+#include <iostream>
+#include <numeric>
+#include <sstream>
+
+#include "../../utils.h"
+
+#include "../../../../runtime/contrib/json/json_node.h"
+#include "../codegen_json/codegen_json.h"
+
+namespace tvm {
+namespace relay {
+namespace contrib {
+
+using namespace backend;
+
+class IlaCNNJSONSerializer : public backend::contrib::JSONSerializer {
+  using JSONGraphNode = tvm::runtime::json::JSONGraphNode;
+  using JSONGraphNodeEntry = tvm::runtime::json::JSONGraphNodeEntry;
+
+ public:
+  IlaCNNJSONSerializer(const std::string& symbol, const Expr& expr) : JSONSerializer(symbol, expr) {}
+
+  std::vector<JSONGraphNodeEntry> VisitExpr_(const CallNode* cn) override {
+    std::string name;
+
+    if (const auto* op_node = cn->op.as<OpNode>()) {
+      name = op_node->name;
+    } else if (const auto* fn = cn->op.as<FunctionNode>()) {
+      auto comp = fn->GetAttr<String>(attr::kComposite);
+      CHECK(comp.defined())
+          << "JSON runtime only supports composite functions.";
+      name = comp.value();
+
+      if (name != "ilacnn.conv2d") {
+        LOG(FATAL) << "Unrecognized pattern: " << name;
+      }
+    } else {
+      LOG(FATAL) << "IlaCNN runtime does not support calls to "
+                 << cn->op->GetTypeKey();
+    }
+    LOG(INFO) << "[Pattern Matching] Find annotated: " << name;
+
+    std::vector<JSONGraphNodeEntry> inputs;
+    for (const auto& arg : cn->args) {
+      auto res = VisitExpr(arg);
+      inputs.insert(inputs.end(), res.begin(), res.end());
+    }
+    auto node = std::make_shared<JSONGraphNode>(name,     /* name_ */
+                                                "kernel", /* op_type_ */
+                                                inputs, 1 /* num_outputs_ */);
+
+    // Note: conv2d has a lot of attrs that are relevant for codegen,
+    // especially the stride size.
+    // However, the pattern matcher will produce patterns in the form of
+    // fn(Compiler="ilacnn") {
+    //   fn(Composuite="ilacnn.conv2d") { nn.conv2d(...) }
+    // }
+    // so we need to reach inside the inner function to get the conv2d attrs (weird, yeah);
+    // see codegen_json.h:SetCallNodeAttribute
+
+    tvm::relay::backend::contrib::OpAttrExtractor extractor(node);
+    auto inner_func = Downcast<Function>(cn->op);
+    auto inner_call = Downcast<Call>(inner_func->body);
+    const Object* inner_call_attr = inner_call->attrs.get();
+    extractor.Extract(const_cast<Object*>(inner_call_attr));
+    return AddNode(node, GetRef<Expr>(cn));
+  }
+};  // class IlaCNNJSONSerializer
+
+runtime::Module IlaCNNCompiler(const ObjectRef& ref) {
+  CHECK(ref->IsInstance<FunctionNode>());
+  auto func = Downcast<Function>(ref);
+  auto func_name = GetExtSymbol(func);
+
+  IlaCNNJSONSerializer serializer(func_name, func);
+  serializer.serialize();
+  std::string graph_json = serializer.GetJSON();
+  auto params = serializer.GetParams();
+
+  const auto* pf = runtime::Registry::Get("runtime.IlaCNNRuntimeCreate");
+  CHECK(pf != nullptr) << "Cannot find IlaCNN runtime module to create";
+  auto mod = (*pf)(func_name, graph_json, params);
+  return mod;
+}
+
+TVM_REGISTER_GLOBAL("relay.ext.ilacnn").set_body_typed(IlaCNNCompiler);
+
+}  // namespace contrib
+}  // namespace relay
+}  // namespace tvm
