86566e8d220cb6d47dcd03773ebddd0ef117f64d
vcanumalla <vishalc@cs.washington.edu>
2021-09-30 16:28:01 -0700

    added conv1d operator to rust bindings

--- rust/tvm/src/ir/relay/attrs/nn.rs
+++ rust/tvm/src/ir/relay/attrs/nn.rs
@@ -28,6 +28,25 @@ type IndexExpr = PrimExpr;
 
 #[repr(C)]
 #[derive(Object, Debug)]
+#[ref_name = "Conv1DAttrs"]
+#[type_key = "relay.attrs.Conv1DAttrs"]
+pub struct Conv1DAttrsNode {
+    pub base: BaseAttrsNode,
+    pub strides: Array<IndexExpr>,
+    pub padding: Array<IndexExpr>,
+    pub dilation: Array<IndexExpr>,
+    // TODO(@gussmith23) groups is "int", what should it be here?
+    pub groups: i32,
+    pub channels: IndexExpr,
+    pub kernel_size: Array<IndexExpr>,
+    pub data_layout: TString,
+    pub kernel_layout: TString,
+    pub out_layout: TString,
+    pub out_dtype: DataType,
+}
+
+#[repr(C)]
+#[derive(Object, Debug)]
 #[ref_name = "Conv2DAttrs"]
 #[type_key = "relay.attrs.Conv2DAttrs"]
 pub struct Conv2DAttrsNode {
