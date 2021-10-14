da94241e8b143b2e0003873211bcea25796f4504
LeeOHzzZ <leeoh12784@gmail.com>
2021-10-14 15:02:28 -0400

    adding support for data alignment for linear_layer by padding

--- tests/python/byo3la/flex_linear.py
+++ tests/python/byo3la/flex_linear.py
@@ -15,19 +15,27 @@ from flexnlp.src.utils import tool
 # define the graph
 # dtype="int8"
 dtype="float32"
-m = 64
-n = 64
+p = 16
+m = 16
+n = 10
 
-shape1 = tvm.relay.TensorType((n, m), dtype=dtype)
-shape2 = tvm.relay.TensorType((n, m), dtype=dtype)
-shape3 = tvm.relay.TensorType((n, ), dtype=dtype)
-x = tvm.relay.var("x", shape1)
-w = tvm.relay.var("w", shape2)
-b = tvm.relay.var("b", shape3)
-matmul = tvm.relay.nn.dense(x, w)
-final = tvm.relay.nn.bias_add(matmul, b)
+x_shape = (p, m)
+w_shape = (n, m)
+b_shape = (n, )
 
-mod = tvm.ir.IRModule.from_expr(final)
+shape1 = tvm.relay.TensorType(x_shape, dtype=dtype)
+shape2 = tvm.relay.TensorType(w_shape, dtype=dtype)
+shape3 = tvm.relay.TensorType(b_shape, dtype=dtype)
+x1 = tvm.relay.var("x1", shape1)
+w1 = tvm.relay.var("w1", shape2)
+b1 = tvm.relay.var("b1", shape3)
+x1 = tvm.relay.var("x2", shape1)
+w2 = tvm.relay.var("w2", shape2)
+b2 = tvm.relay.var("b2", shape3)
+y1 = tvm.relay.nn.bias_add(tvm.relay.nn.dense(x1, w1), b1)
+y2 = tvm.relay.nn.bias_add(tvm.relay.nn.dense(y1, w2), b2)
+
+mod = tvm.ir.IRModule.from_expr(y1)
 print(mod)
 
 
@@ -36,6 +44,8 @@ pattern_table = ilaflex.pattern_table()
 mod = tvm.relay.transform.MergeComposite(pattern_table)(mod)
 mod = tvm.relay.transform.AnnotateTarget(["ilaflex"])(mod) 
 mod = tvm.relay.transform.PartitionGraph()(mod) 
+print("mod after annotation\n")
+print(mod)
 
 print("[Python] Compile with the 3LA extension")
 target = tvm.target.create('llvm')
@@ -50,20 +60,19 @@ ctx = tvm.cpu()
 runtime_exec = graph_runtime.create(graph, lib, ctx)
 
 coef = 1
-# x_np = coef * np.random.uniform(0, 1, size=(n, m)).astype(np.float32)
-# y_np = coef * np.random.uniform(0, 1, size=(n, m)).astype(np.float32)
-# z_np = coef * np.random.uniform(0, 1, size=(n,)).astype(np.float32)
-x_np = np.random.uniform(-2, 2, size=(n, m)).astype(dtype)
-y_np = np.random.uniform(-2, 2, size=(n, m)).astype(dtype)
-z_np = np.random.uniform(-2, 2, size=(n,)).astype(dtype)
+
+if dtype == "int8":
+    x_np = np.random.uniform(-128, 128, size=x_shape).astype(dtype)
+    y_np = np.random.uniform(-128, 128, size=w_shape).astype(dtype)
+    z_np = np.random.uniform(-128, 128, size=b_shape).astype(dtype)
+else:
+    x_np = coef * np.random.uniform(0, 1, size=x_shape).astype(dtype)
+    y_np = coef * np.random.uniform(0, 1, size=w_shape).astype(dtype)
+    z_np = coef * np.random.uniform(0, 1, size=b_shape).astype(dtype)
 
 
 ref = np.add(np.matmul(x_np, np.transpose(y_np)), z_np)
 print(ref)
-# x_np = coef * np.random.random_sample((n, m), dtype = np.float32)
-# x_np = np.array([[1, 2], [3, 4]], dtype = np.float32)
-# y_np = coef * np.random.random_sample((n, m), dtype = np.float32)
-# z_np = coef * np.random.random_sample((n,), dtype = np.float32)
 x_tvm = tvm.nd.array(x_np)
 y_tvm = tvm.nd.array(y_np)
 z_tvm = tvm.nd.array(z_np)
@@ -80,9 +89,7 @@ output = output.astype(np.float32)
 print("[Python] Done")
 
 tl = tool()
-ref, bias = tl.get_adpfloat_bias(ref)
 err_out, err_ref = tl.cal_error(output, ref)
-# print(err_out, err_ref)
 print("relative error: {:5.5%} vs. output, {:5.5%} vs. ref".format(err_out, err_ref))
 print('output: {}'.format(output.shape))
 print(output)
